81e22a865e9f8f77cba14a407de23b3cd2a691a0
Tony D <astowny@gmail.com>
2025-02-25 17:39:33 +0100

    remove cors issue

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 7832;
 
+// Configuration des en-têtes CORS pour permettre les requêtes cross-origin
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permet l'accès depuis n'importe quelle origine
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  // Gestion des requêtes OPTIONS (pre-flight)
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
 // Endpoint qui retourne "Hello World" en JSON avec des informations supplémentaires
 app.get('/', (req, res) => {
   res.json({
